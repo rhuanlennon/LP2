e2648c074b519a79b94ded22453ce05aaa6631c8
Rhuan Resende <rhuan.resende@g.globo>
2022-02-15 10:27:35 -0300

    change type int to float largura and altura

--- 03-Classes/Grades.c
+++ 03-Classes/Grades.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 typedef struct {
-	int largura, altura;
+	float largura, altura;
 	int qtdHorizontais,qtdVerticais;
 } Grades;
 
 void print(Grades* g){
-	printf("Grades com %dpx de largura, %dpx de altura, %d divisores horizontais e %d divisores verticais \n", g->largura, g->altura, g->qtdHorizontais, g -> qtdVerticais);
+	printf("Grades com %.1f px de largura, %.1f px de altura, %d divisores horizontais e %d divisores verticais \n", g->largura, g->altura, g->qtdHorizontais, g -> qtdVerticais);
 }
 
 void main(void){
-	Grades g1 = {100, 100, 20, 20 };
+	Grades g1 = {150.5, 120.6, 20, 20 };
 	print(&g1);
 }
