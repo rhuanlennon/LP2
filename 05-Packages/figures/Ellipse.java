938f1c07409c8d41a1e81c89e421125275ab6b15
Rhuan Resende <rhuan.resende@g.globo>
2022-02-23 14:11:24 -0300

    change private types

--- 05-Packages/figures/Ellipse.java
+++ 05-Packages/figures/Ellipse.java
@@ -5,8 +5,8 @@ import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 
 public class Ellipse {
-    int x, y;
-    int w, h;
+    private int x, y;
+    private int w, h;
 
     public Ellipse (int x, int y, int w, int h) {
         this.x = x;
