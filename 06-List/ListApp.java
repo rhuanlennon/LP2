b0e3f32fc8811a7764ca8ade87774061a3c136e1
Rhuan Resende <rhuan.resende@g.globo>
2022-03-05 22:20:52 -0300

    class Rect

--- 06-List/ListApp.java
+++ 06-List/ListApp.java
@@ -35,3 +35,24 @@ class ListRect extends JFrame {
 
     }
 }
+
+class Rect {
+    int x, y;
+    int w, h;
+
+    Rect (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+    void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+}
