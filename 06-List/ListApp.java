dbb308fce4565b8509d112edcc8fbf16dea5d014
Rhuan Resende <rhuan.resende@g.globo>
2022-03-07 18:35:34 -0300

    ident

--- 06-List/ListApp.java
+++ 06-List/ListApp.java
@@ -123,9 +123,9 @@ class Ellipse {
 
     void print () {
         System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+        this.w, this.h, this.x, this.y);
     }
-
+    
     void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
