0b30dc9af012e0ef5c2c367aee51073ffeaf4987
Rhuan Resende <rhuan.resende@g.globo>
2022-03-22 22:37:36 -0300

    create Poligono class

---
+++ Project1/figures/Poligono.java
@@ -0,0 +1,27 @@
+package figures;
+
+import java.awt.*;
+
+public class Poligono extends Figure {
+	public Color fundo;
+	private Polygon p = new Polygon();
+
+	public Poligono(int x, int y, int w, int h) {
+		super(x, y, w, h);
+        
+        p.npoints = 3;
+        p.xpoints[0] = (int) (x + w/2);
+        p.xpoints[1] = x + w;
+        p.xpoints[2] = (int) (x + w*0.75);
+        
+        p.ypoints[0] = y;
+        p.ypoints[1] = (int) (y + h*0.40);
+        p.ypoints[2] = y+h;
+    }
+
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+        g2d.drawPolygon(p.xpoints, p.ypoints, p.npoints);
+	}
+
+}
