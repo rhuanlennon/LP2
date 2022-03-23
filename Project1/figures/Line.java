f6db74b1705c6dfabd951d7fb7027069e56add14
Rhuan Resende <rhuan.resende@g.globo>
2022-03-22 22:42:07 -0300

    modify, adding x with h and y with h

--- Project1/figures/Line.java
+++ Project1/figures/Line.java
@@ -10,6 +10,6 @@ public class Line extends Figure {
     
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawLine(this.x,this.y, this.w,this.h);
+        g2d.drawLine(this.x, this.y, this.x+w, this.y+h);
     }
 }
