b9c547c826f8414c6226cc59d6740c4f5d300c39
Rhuan Resende <rhuan.resende@g.globo>
2022-03-29 20:01:31 -0300

    adding colorFill and colorDraw, fillOval

--- Project1/figures/Ellipse.java
+++ Project1/figures/Ellipse.java
@@ -4,13 +4,17 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 public class Ellipse extends Figure {
-    
-    public Ellipse(int x, int y, int w, int h) {
-        super(x, y, w, h);
+    public Color colorDraw;
+    public Ellipse(int x, int y, int w, int h, Color colorFill, Color colorDraw) {
+        super(x, y, w, h, colorFill);
+        this.colorDraw = colorDraw;
     }
     
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(colorDraw);
+        g2d.fillOval(this.x, this.y, this.w, this.h);
+        g2d.setColor(colorFill);
         g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
     }
     
