bce4a5de2ddf60e3f9f7b5e857761726c43c6c13
Rhuan Resende <rhuan.resende@g.globo>
2022-03-29 20:02:03 -0300

    adding colorFill for the line, isn't necessary colorDraw

--- Project1/figures/Line.java
+++ Project1/figures/Line.java
@@ -4,12 +4,13 @@ import java.awt.*;
 
 public class Line extends Figure {
     
-    public Line(int x, int y, int w, int h) {
-        super(x, y, w, h);
+    public Line(int x, int y, int w, int h, Color colorFill) {
+        super(x, y, w, h, colorFill);
     }
     
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(colorFill);
         g2d.drawLine(this.x, this.y, this.x+w, this.y+h);
     }
 }
