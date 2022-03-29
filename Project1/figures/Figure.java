ef7b507ce3ffde38343e4c832e68f3f76631f7a7
Rhuan Resende <rhuan.resende@g.globo>
2022-03-29 20:00:20 -0300

    adding colorFill

--- Project1/figures/Figure.java
+++ Project1/figures/Figure.java
@@ -1,16 +1,22 @@
 package figures;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 import java.awt.Graphics;
 
 public abstract class Figure {
-    int x, y;
-    int w, h;
+    public int x, y;
+    public int w, h;
+    public Color colorFill;
 
-    public Figure(int x, int y, int w, int h) {
+    public Figure(int x, int y, int w, int h, Color colorFill) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.colorFill = colorFill;
     }
     public abstract void paint (Graphics g);
 }
