01422602a0a2cb22e1a94159637222ed41903949
Rhuan Resende <rhuan.resende@g.globo>
2022-03-29 20:03:17 -0300

    adding colorDraw and colorFill, fillRect

--- Project1/figures/Rect.java
+++ Project1/figures/Rect.java
@@ -3,13 +3,18 @@ package figures;
 import java.awt.*;
 
 public class Rect extends Figure {
+    public Color colorDraw;
     
-    public Rect(int x, int y, int w, int h) {
-        super(x, y, w, h);
+    public Rect(int x, int y, int w, int h, Color colorFill, Color colorDraw) {
+        super(x, y, w, h, colorFill);
+        this.colorDraw = colorDraw;
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(colorFill);
+        g2d.drawRect(this.x,this.y,this.w,this.h);
+        g2d.setColor(colorDraw);
+        g2d.fillRect(this.x,this.y,this.w,this.h);
     }
 }
